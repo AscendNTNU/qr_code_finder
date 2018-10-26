494e3fb166ae0fbd594e53127c74d82215f603a5
Torjus <torjus.bakkene@gmail.com>
2018-10-27 00:20:15 +0200

    Now reads images from /cv_camera/image_raw/ and publishes to /qr_code_finder/output_video

--- src/main.cc
+++ src/main.cc
@@ -1,39 +1,56 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <cmath>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
+static const std::string OPENCV_WINDOW = "Image window";
 using namespace cv;
 using namespace std;
 
-// Initializing global variables
-Mat src;
-Mat src_gray;
-int thresh = 255;
-
-/** 
- * Main function
- * Displays the most QR-like contour on top of source image 
-**/
-int main(int argc, char **argv)
+class ImageConverter
 {
-    // Load source image
-    src = imread(argv[1], 1);
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscribe to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/cv_camera/image_raw", 1,
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/qr_code_finder/output_video", 1);
+
+    // cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    // cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  // Finds a QR code in the image
+  cv::Mat findQR(cv::Mat src)
+{
+    // Holds the processed image
+    cv::Mat src_gray;
 
     // Convert image to gray and blur it
-    cvtColor(src, src_gray, CV_BGR2GRAY);
-    blur(src_gray, src_gray, Size(3, 3));
+    cv::cvtColor(src, src_gray, CV_BGR2GRAY);
+    cv::blur(src_gray, src_gray, Size(3, 3));
 
-    Mat canny_output;
+    cv::Mat canny_output;
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
 
     // Detect edges using canny
-    Canny(src_gray, canny_output, thresh, thresh * 2, 3);
+    cv::Canny(src_gray, canny_output, 255, 255 * 2, 3);
     // Find contours
-    findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_TC89_KCOS, Point(0, 0));
+    cv::findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_TC89_KCOS, Point(0, 0));
 
     // Stores weights of contours
     vector<int> weights;
@@ -41,13 +58,13 @@ int main(int argc, char **argv)
     // Determine the weighting and store it
     for (int i = 0; i < contours.size(); i++)
     {
-        approxPolyDP(contours[i], contours[i], 4, true);
+        cv::approxPolyDP(contours[i], contours[i], 4, true);
         int shapeweight = abs((int)(4 - contours[i].size()))+1;
         shapeweight *= 2; //Weigh shape more
-        int areaweight = (int)contourArea(contours[i]);
+        int areaweight = (int)cv::contourArea(contours[i]);
         weights.push_back(areaweight / shapeweight);
-        cout << shapeweight << ", " << areaweight << " | "<< weights[i] << endl;
     }
+
 
     int bestContour = 0;
     int bestWeight = 0;
@@ -62,14 +79,51 @@ int main(int argc, char **argv)
 
     //Draw the contour on the source image
     Scalar color = Scalar(0, 0, 255);
-    drawContours(src, contours, bestContour, color, 2, 8, hierarchy, 0, Point());
-    cout <<"Edges: " << contours[bestContour].size() << endl;
-    cout <<"Weight: " << weights[bestContour] << endl;
+    try
+    {
+      cv::drawContours(src, contours, bestContour, color, 2, 8, hierarchy, 0, Point());
+    }
+    catch (cv::Exception e)
+    {
 
-    /// Show in a window
-    cv::resize(src, src, cv::Size(), 0.3, 0.3);
-    namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-    imshow("Contours", src);
-    waitKey(0);
-    return (0);
+    }
+    // Return marked image
+    return(src);
+}
+
+  /*
+   Callback when receiving image
+   Republishes image with most likely QR marked
+  */
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Mark image with most likely QR code
+    cv_ptr->image = findQR(cv_ptr->image);
+
+    // Update GUI Window
+    // cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    // cv::waitKey(3);
+
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "qr_code_finder");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
 }
