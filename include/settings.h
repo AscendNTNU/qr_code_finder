d2bc44612a7060f804739d9a6333bc0a9b50a6c4
Torjus <torjus.bakkene@gmail.com>
2019-01-30 17:44:00 +0100

    Initial code commit

---
+++ include/settings.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+
+namespace settings
+{
+/*
+    General
+*/
+const bool DRAW_ALL_POINTS = false;
+const bool DRAW_POINTS = false;
+const bool DRAW_MEAN_POINT = false;
+const bool DRAW_LINES = false;
+const bool DRAW_BOUNDS = false;
+
+
+
+/* 
+    Point generation
+*/
+
+// Blur kernel size
+const cv::Size BLUR_KERNEL_SIZE = cv::Size(3, 3);
+
+// Harris block size
+const int HARRIS_BLOCK_SIZE = 2;
+
+// Harris kernel size
+const int HARRIS_KERNEL_SIZE = 3;
+
+// The brightness of a pixel on normalized Harris image needed to count as a corner point
+const int CORNER_POINT_THRESHOLD = 180;
+
+// The amount of corner points allowed before image is scrapped
+const int MAX_ALLOWED_POINTS = 500;
+
+// Multiplier for getting the maximum allowad distance from the mean point. 
+const float MEAN_POINT_DISTANCE_MULTIPLIER = 1.4;
+
+/* 
+    QR validation
+*/
+
+// The amount lines are allowed to differ from horizontal or vertical before the candidate is discarded.
+const float ANGLE_MARGIN = 0.01;
+
+// The amount cropped images are allowed to differ from square before the candidate is discarded.
+const float SQUARENESS_MARGIN = 0.2;
+
+// Minimum amount of rows and columns required to keep the candidate
+const int MIN_CROP_ROWS = 50;
+
+} // namespace settings
---
+++ src/evaluation.cc
@@ -0,0 +1,100 @@
+#include "evaluation.h"
+#include "settings.h"
+#include "debug.h"
+using namespace cv;
+using namespace std;
+
+bool checkSquareness(cv::Mat image)
+{
+    double squareScore = static_cast<double>(image.cols) / static_cast<double>(image.rows);
+    if (abs(squareScore) >= 1 + settings::SQUARENESS_MARGIN || abs(squareScore) <= 1 - settings::SQUARENESS_MARGIN)
+    { //
+        return false;
+    }
+    return true;
+}
+
+bool checkImageDetail(cv::Mat image, std::vector<cv::Point> points)
+{
+    if (image.cols < settings::MIN_CROP_ROWS || image.rows < settings::MIN_CROP_ROWS)
+    { // Too small image, not enough detail
+        return false;
+    }
+    else if (points.size() < 8 || points.size() > 200)
+    { // Too few corners
+        // TODO: Switch this out with something that checks inside the cropped area
+        return false;
+    }
+    return true;
+}
+
+bool checkImageLines(cv::Mat image)
+{
+    Mat sobelFrame, sobelFrameNorm, outFrameGray, outFrameBin;
+    cvtColor(image, outFrameGray, CV_BGR2GRAY);
+
+    // Detect horizontal lines
+    // Sobel(outFrameGray, sobelFrame, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+    Canny(outFrameGray, sobelFrame, 80, 255, 3, false);
+    convertScaleAbs(sobelFrame, sobelFrameNorm);
+
+    // sobelFrameNorm.copyTo(outFrame);
+    vector<Vec2f> lines;
+    HoughLines(sobelFrameNorm, lines, 1, CV_PI / 180, 40);
+
+    if (lines.size() == 0)
+    {
+#ifdef DEBUG
+        cerr << "TOO FEW LINES" << endl;
+#endif //DEBUG
+        return false;
+    }
+#ifdef DEBUG
+    cout << "NUMBER OF LINES: " << lines.size() << endl;
+#endif //DEBUG
+    int counted = 0;
+    int wrongAngleCount = 0;
+    for (Vec2f l : lines)
+    {
+        if (l[0] < 20)
+            continue; // Line is too short to use
+#ifdef DEBUG
+        cout << "LINE: " << l[0] << " | " << l[1] << endl;
+#endif //DEBUG
+        float theta = l[1];
+
+        bool lineAngleIsWrong = (abs(0 - theta) > settings::ANGLE_MARGIN) &&
+                                (abs(CV_PI - theta) > settings::ANGLE_MARGIN) &&
+                                (abs(CV_PI / 2 - theta) > settings::ANGLE_MARGIN) &&
+                                (abs(CV_PI * 3 / 2 - theta) > settings::ANGLE_MARGIN);
+
+        counted++;
+        if (lineAngleIsWrong)
+        {
+            wrongAngleCount++;
+        }
+    }
+
+    if (settings::DRAW_LINES)
+    {
+        // Draw the lines
+        for (size_t i = 0; i < lines.size(); i++)
+        {
+            float rho = lines[i][0], theta = lines[i][1];
+            Point pt1, pt2;
+            double a = cos(theta), b = sin(theta);
+            double x0 = a * rho, y0 = b * rho;
+            pt1.x = cvRound(x0 + 1000 * (-b));
+            pt1.y = cvRound(y0 + 1000 * (a));
+            pt2.x = cvRound(x0 - 1000 * (-b));
+            pt2.y = cvRound(y0 - 1000 * (a));
+            line(image, pt1, pt2, Scalar(0, 0, 255), 1);
+        }
+    }
+
+    if (wrongAngleCount >= counted / 2)
+    {
+        return false;
+    }
+    return true;
+}
