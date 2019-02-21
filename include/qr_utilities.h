27a19419be40391dae294e3bf8a8fb8a4ec053d5
Torjus <torjus.bakkene@gmail.com>
2019-02-21 13:10:32 +0100

    Code cleanup

--- include/qr_utilities.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "opencv2/opencv.hpp"
-
-cv::Size_<int> operator/(const cv::Size_<int> &leftSize, const cv::Size_<int> &rightSize);
--- src/qr_utilities.cc
+++
@@ -1,6 +0,0 @@
-#include "qr_utilities.h"
-
-cv::Size_<int> operator/(const cv::Size_<int> &leftSize, const cv::Size_<int> &rightSize)
-{
-    return cv::Size_<int>(leftSize.width / rightSize.width, leftSize.height / rightSize.height);
-}
