6dbff0fbc1b402b43c1b7f33e5aec0becc2697fa
Torjus <torjus.bakkene@gmail.com>
2019-04-04 17:52:16 +0200

    changed camera number again

--- launch/set_camera_parameters.sh
+++ launch/set_camera_parameters.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-v4l2-ctl -d /dev/video1 --set-ctrl exposure_auto=1
-v4l2-ctl -d /dev/video1 --set-ctrl exposure_absolute=80
+v4l2-ctl -d /dev/video0 --set-ctrl exposure_auto=1
+v4l2-ctl -d /dev/video0 --set-ctrl exposure_absolute=80
+v4l2-ctl -d /dev/video0 -p 5
 
